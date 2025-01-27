688bd22af4291fcd9806be20df13e4ae3504f854
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2025-01-27 18:47:10 +0330

    fixed building issue

--- components/modules/Header.jsx
+++ components/modules/Header.jsx
@@ -2,22 +2,39 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MobileMenu from "./MobileMenu";
 import HamburgerBtn from "./HamburgerBtn";
 
 function Header() {
   const [openMenu, setOpenMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const imageSrc =
+    windowWidth < 768
+      ? "/assets/images/banner-logo-mobile.png"
+      : "/assets/images/banner-logo.jpeg";
+
   return (
     <header className="fixed w-full flex flex-col items-start divide-y-4 divide-dark-brown bg-light-green z-50 ">
       <HamburgerBtn setOpenMenu={setOpenMenu} openMenu={openMenu} />
       <Image
         className="h-16 md:h-32 self-center"
-        src={
-          window.innerWidth < 768
-            ? "/assets/images/banner-logo-mobile.png"
-            : "/assets/images/banner-logo.jpeg"
-        }
+        src={imageSrc}
         alt="banner"
         priority
         width={1000}
