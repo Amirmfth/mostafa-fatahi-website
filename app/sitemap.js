e1ca001bdf6654bf350554f82d9f6aa90ead9982
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2025-01-28 12:48:41 +0330

    fixed lasmod issue in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,16 @@
 export default async function sitemap() {
-    const staticRoutes = ["", "/about-me", "/articles", "/info", "/pricing", "/questions"];
+  const staticRoutes = [
+    "",
+    "/about-me",
+    "/articles",
+    "/info",
+    "/pricing",
+    "/questions",
+  ];
 
-    const routes = staticRoutes.map((route) => ({
-        url: `https://mostafa-fatahi.ir${route}`,
-        lastModified: new Date().toString(),
-    }));
-
-    return [...routes];
+  const routes = staticRoutes.map((route) => ({
+    url: `https://mostafa-fatahi.ir${route}`,
+    lastModified: "2025-01-27T00:00:00Z",
+  }));
+  return [...routes];
 }
